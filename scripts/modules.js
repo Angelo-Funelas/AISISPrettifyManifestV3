0baf111839244aadb71c0cdc7392ffa1b6e32d3e
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-07-07 16:11:05 +0800

    add new homepage layout

--- scripts/modules.js
+++ scripts/modules.js
@@ -23,3 +23,21 @@ function gotoBottom(id){
     var element = document.getElementById(id);
     element.scrollTop = element.scrollHeight - element.clientHeight;
 }
+function parseTable(table) {
+    var rows = table.querySelector('tbody').querySelectorAll('tr')
+    var parsedTable = []
+    for (let i = 0; i< rows.length;i++) {
+        cells = rows[i].querySelectorAll('td')
+        var parsedRow = []
+        for (let cell of cells) {
+            var links = cell.querySelectorAll('a')
+            if (links.length > 0) {
+                parsedRow.push([cell.innerText, links[0]])
+            } else {
+                parsedRow.push([cell.innerText])
+            }
+        }
+        parsedTable.push(parsedRow)
+    }
+    return parsedTable
+}
