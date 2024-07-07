5fd4efa9b4b5d2e8436a5034236bc83e1ac085a3
Angelo-Funelas <angelomartinfunelas@gmail.com>
2024-07-07 19:47:52 +0800

    revert document_start changes and add link to header

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,45 +1,46 @@
 const icon_map = [`google`,`ips`,`print`,`curriculum`,`studentinfo`,`classes`,`grades`,`holdorder`,`change`,`schedule`,`password`,`list`]
 chrome.storage.local.get(['disable_homepage'], function(result) {
     if (((window.location.href == 'https://aisis.ateneo.edu/j_aisis/welcome.do') || (window.location.href == 'https://aisis.ateneo.edu/j_aisis/login.do')) && !result.disable_homepage) {
-        document.addEventListener('DOMContentLoaded', function() {
-            var sitemap = document.getElementsByTagName('table')[11]
-            parsedTable = parseTable(sitemap)
-            if (parsedTable[0][0] == 'Site Map') {
-                sitemap.classList.add('hidden')
-                var newSiteMap = document.createElement('div')
-                newSiteMap.id = 'ap-sitemap'
-                sitemap.parentElement.append(newSiteMap)
-    
-                for (let i=1;i<parsedTable.length;i++) {
-                    var navData = parsedTable[i]
-                    var link = document.createElement('a')
-                    var navElement = document.createElement('div')
-                    var elIcon = document.createElement('img')
-                    var elHeading = document.createElement('h3')
-                    var elDesc = document.createElement('p')
-    
-                    if (i-1<icon_map.length) {
-                        elIcon.src = chrome.runtime.getURL(`/images/icons/${icon_map[i-1]}.png`)
-                    }
-                    elHeading.innerText = navData[0][0]
-                    elDesc.innerText = navData[1]
-                    navElement.append(elIcon,elHeading,elDesc)
-                    if (navData[0].length>1) {
-                        link.href = navData[0][1].href
-                        link.appendChild(navElement)
-                        newSiteMap.appendChild(link)
-                    } else {
-                        newSiteMap.appendChild(navElement)
-                    }
+        var sitemap = document.getElementsByTagName('table')[11]
+        parsedTable = parseTable(sitemap)
+        if (parsedTable[0][0] == 'Site Map') {
+            sitemap.classList.add('hidden')
+            var newSiteMap = document.createElement('div')
+            newSiteMap.id = 'ap-sitemap'
+            sitemap.parentElement.append(newSiteMap)
+
+            for (let i=1;i<parsedTable.length;i++) {
+                var navData = parsedTable[i]
+                var link = document.createElement('a')
+                var navElement = document.createElement('div')
+                var elIcon = document.createElement('img')
+                var elHeading = document.createElement('h3')
+                var elDesc = document.createElement('p')
+
+                if (i-1<icon_map.length) {
+                    elIcon.src = chrome.runtime.getURL(`/images/icons/${icon_map[i-1]}.png`)
                 }
-                var header = document.createElement('div')
-                var headerh = document.createElement('h2')
-                var headerp = document.createElement('p')
-                headerh.innerText = 'AISIS Prettify'
-                headerp.innerText = 'by Gelo Funelas'
-                header.append(headerh,headerp)
-                newSiteMap.append(header)
+                elHeading.innerText = navData[0][0]
+                elDesc.innerText = navData[1]
+                navElement.append(elIcon,elHeading,elDesc)
+                if (navData[0].length>1) {
+                    link.href = navData[0][1].href
+                    link.appendChild(navElement)
+                    newSiteMap.appendChild(link)
+                } else {
+                    newSiteMap.appendChild(navElement)
+                }
             }
-        })
+            var header = document.createElement('div')
+            var headerh = document.createElement('h2')
+            var headera = document.createElement('a')
+            var headerp = document.createElement('p')
+            headerh.innerText = 'AISIS Prettify'
+            headerp.innerText = 'by Gelo Funelas'
+            headera.href = 'https://gemplo.com'
+            headera.appendChild(headerp)
+            header.append(headerh,headera)
+            newSiteMap.append(header)
+        }
     }
 })
--- scripts/schedule.js
+++ scripts/schedule.js
@@ -4,95 +4,93 @@ function to12hr(hr) {
 }
 chrome.storage.local.get(['disable_schedule'], function(result) {
     if (window.location.href == 'https://aisis.ateneo.edu/j_aisis/J_VMCS.do' && !result.disable_schedule) {
-        document.addEventListener('DOMContentLoaded', function() {
-            var table = document.getElementsByTagName('table')[15]
-            var rows = table.querySelector('tbody').querySelectorAll('tr')
-            var parsedTable = []
-            for (let i = 0; i< rows.length;i++) {
-                cells = rows[i].querySelectorAll('td')
-                var parsedRow = []
-                for (let cell of cells) {
-                    parsedRow.push(cell.innerText)
-                }
-                parsedTable.push(parsedRow)
+        var table = document.getElementsByTagName('table')[15]
+        var rows = table.querySelector('tbody').querySelectorAll('tr')
+        var parsedTable = []
+        for (let i = 0; i< rows.length;i++) {
+            cells = rows[i].querySelectorAll('td')
+            var parsedRow = []
+            for (let cell of cells) {
+                parsedRow.push(cell.innerText)
             }
-            console.log(parsedTable)
-    
-            const subjectColors = ['3DC2EC', '4B70F5', '4C3BCF', '402E7A', '3ABEF9', '3572EF','050C9C', '153448', '3C5B6F']
-            var subjects = new Map()
-            var gridTable = []
+            parsedTable.push(parsedRow)
+        }
+        console.log(parsedTable)
+
+        const subjectColors = ['3DC2EC', '4B70F5', '4C3BCF', '402E7A', '3ABEF9', '3572EF','050C9C', '153448', '3C5B6F']
+        var subjects = new Map()
+        var gridTable = []
+        for (let i=0;i<parsedTable[0].length;i++) {
+            var col = []
+            var prevSubj = ' '
+            for (let j=1;j<parsedTable.length;j++) {
+                if (i==0) {
+                    col.push([parsedTable[j][i],j+1])
+                } else {
+                    var curSubj = parsedTable[j][i]
+                    if (prevSubj!==curSubj) {
+                        if (prevSubj!==" ") {
+                            col.push([prevSubj,subjStart+1,j+1])
+                            if (!subjects.has(prevSubj)) {
+                                subjects.set(prevSubj, subjects.size)
+                            }
+                        }
+                        if (curSubj!==" ") {
+                            subjStart = j
+                        }
+                        prevSubj = curSubj
+                    }
+                }
+            }
+            gridTable.push(col)
+        }
+        console.log(gridTable)
+        console.log(subjects)
+        if (parsedTable[0].length == 7) {
+            var gridSchedule = document.createElement('div')
+            gridSchedule.id = "prettyGrid"
+            gridSchedule.style.gridTemplateRows = `64px repeat(${gridTable[0].length-1}, 32px)`
+            for (let i=0;i<gridTable[0].length;i++) {
+                var gridLine = document.createElement('div')
+                gridLine.className = 'gridLine'
+                gridLine.style.gridRow = i+1
+                gridSchedule.append(gridLine)
+            }
             for (let i=0;i<parsedTable[0].length;i++) {
-                var col = []
-                var prevSubj = ' '
-                for (let j=1;j<parsedTable.length;j++) {
+                var heading = document.createElement('p')
+                heading.className = 'headCell'
+                heading.style.gridColumn = i+1
+                heading.innerText = parsedTable[0][i]
+                gridSchedule.append(heading)
+            }
+            for (let i=0;i<gridTable.length;i++) {
+                for (let j=0;j<gridTable[i].length;j++) {
                     if (i==0) {
-                        col.push([parsedTable[j][i],j+1])
+                        if (j%2==0) {
+                            var cont = document.createElement('div')
+                            var time = document.createElement('p')
+                            cont.className = 'timeCell'
+                            var timeStr = gridTable[0][j][0].split("-")[0]
+                            time.innerText = to12hr(timeStr.slice(0, -2)) + ':' + timeStr.slice(-2);
+                            cont.style.gridRow = gridTable[0][j][1]
+                            cont.appendChild(time)
+                            gridSchedule.append(cont)
+                        }
                     } else {
-                        var curSubj = parsedTable[j][i]
-                        if (prevSubj!==curSubj) {
-                            if (prevSubj!==" ") {
-                                col.push([prevSubj,subjStart+1,j+1])
-                                if (!subjects.has(prevSubj)) {
-                                    subjects.set(prevSubj, subjects.size)
-                                }
-                            }
-                            if (curSubj!==" ") {
-                                subjStart = j
-                            }
-                            prevSubj = curSubj
-                        }
+                        var classBlock = document.createElement('div')
+                        classBlock.className = 'classCell'
+                        classBlock.style.backgroundColor = `#${subjectColors[subjects.get(gridTable[i][j][0])]}`
+                        classBlock.innerText = gridTable[i][j][0]
+                        classBlock.style.gridColumn = i+1
+                        classBlock.style.gridRowStart = gridTable[i][j][1]
+                        classBlock.style.gridRowEnd = gridTable[i][j][2]
+                        gridSchedule.append(classBlock)
                     }
                 }
-                gridTable.push(col)
+            
             }
-            console.log(gridTable)
-            console.log(subjects)
-            if (parsedTable[0].length == 7) {
-                var gridSchedule = document.createElement('div')
-                gridSchedule.id = "prettyGrid"
-                gridSchedule.style.gridTemplateRows = `64px repeat(${gridTable[0].length-1}, 32px)`
-                for (let i=0;i<gridTable[0].length;i++) {
-                    var gridLine = document.createElement('div')
-                    gridLine.className = 'gridLine'
-                    gridLine.style.gridRow = i+1
-                    gridSchedule.append(gridLine)
-                }
-                for (let i=0;i<parsedTable[0].length;i++) {
-                    var heading = document.createElement('p')
-                    heading.className = 'headCell'
-                    heading.style.gridColumn = i+1
-                    heading.innerText = parsedTable[0][i]
-                    gridSchedule.append(heading)
-                }
-                for (let i=0;i<gridTable.length;i++) {
-                    for (let j=0;j<gridTable[i].length;j++) {
-                        if (i==0) {
-                            if (j%2==0) {
-                                var cont = document.createElement('div')
-                                var time = document.createElement('p')
-                                cont.className = 'timeCell'
-                                var timeStr = gridTable[0][j][0].split("-")[0]
-                                time.innerText = to12hr(timeStr.slice(0, -2)) + ':' + timeStr.slice(-2);
-                                cont.style.gridRow = gridTable[0][j][1]
-                                cont.appendChild(time)
-                                gridSchedule.append(cont)
-                            }
-                        } else {
-                            var classBlock = document.createElement('div')
-                            classBlock.className = 'classCell'
-                            classBlock.style.backgroundColor = `#${subjectColors[subjects.get(gridTable[i][j][0])]}`
-                            classBlock.innerText = gridTable[i][j][0]
-                            classBlock.style.gridColumn = i+1
-                            classBlock.style.gridRowStart = gridTable[i][j][1]
-                            classBlock.style.gridRowEnd = gridTable[i][j][2]
-                            gridSchedule.append(classBlock)
-                        }
-                    }
-                
-                }
-                table.parentElement.append(gridSchedule)
-                table.style.display = 'none'
-            }
-        })
+            table.parentElement.append(gridSchedule)
+            table.style.display = 'none'
+        }
     }
 })
